e7915e1ddfcbb3efeb7f04b3a01b7f665cd9b876
Cato Cannizzo <cannizzo@uw.edu>
2026-02-23 22:31:08 -0800

    Fixed miscalles and stack overflows from recursive calling

--- GeneralFunctions.py
+++ GeneralFunctions.py
@@ -1,46 +1,41 @@
 def promptUser(typed:type = None, userPrompt: str = None, aboveX:int = None,underY:int = None, ynChoice: bool =False, inList=[]):
     """Returns value from prompted user, optional arg to enforce type return of prompt, and to give specific prompt"""
-    kwargs = {
-        'typed':typed, 
-        'userPrompt':userPrompt, 
-        'aboveX':aboveX,
-        'underY':underY, 
-        'ynChoice': ynChoice
-    }
-    if userPrompt:
-        print(f"{userPrompt}")
-    userInput = input()
 
-    if typed is not None:
-        try:
-            userInput = typed(userInput)
-        except ValueError:
-            print(f'ValueError: value inputted must be of {typed.__name__} type')
-            return promptUser(**kwargs) 
-        except Exception as e:
-            print(f'Unknown Error: {type(e).__name__} - {e}')
-            return promptUser(**kwargs)   
-        
-    if isinstance(userInput, int):
-        lenInput = userInput
-        errorNoun = "value"
-    else:
-        lenInput = len(userInput)
-        errorNoun = "length"
+    while True:
+        if userPrompt:
+            print(f"{userPrompt}")
+        userInput = input()
 
-        if aboveX is not None and not (lenInput > aboveX):
-                print(f'ValueError: {errorNoun} of input must be greater than {aboveX}')
-                return promptUser(**kwargs) 
-        if underY is not None and not (lenInput < underY):
-            print(f'ValueError: {errorNoun} of input must be less than {underY}')
-            return promptUser(**kwargs)
-        if ynChoice:
-            if  userInput.lower() not in ['y','n']:
-                print(f'ValueError: value inputted must be y or n only.')
-                return promptUser(**kwargs)
-    if inList:
-        if userInput.lower() not in inList:
-                print(f'ValueError: value inputted must one of {inList}.')
-                return promptUser(**kwargs)   
-    return userInput
+        if typed is not None:
+            try:
+                userInput = typed(userInput)
+            except ValueError:
+                print(f'ValueError: value inputted must be of {typed.__name__} type')
+                continue 
+            except Exception as e:
+                print(f'Unknown Error: {type(e).__name__} - {e}')
+                continue 
+            
+        if isinstance(userInput, int):
+            lenInput = userInput
+            errorNoun = "value"
+        else:
+            lenInput = len(userInput)
+            errorNoun = "length"
+
+            if aboveX is not None and not (lenInput > aboveX):
+                    print(f'ValueError: {errorNoun} of input must be greater than {aboveX}')
+                    continue 
+            if underY is not None and not (lenInput < underY):
+                print(f'ValueError: {errorNoun} of input must be less than {underY}')
+                continue
+            if ynChoice:
+                if  userInput.lower() not in ['y','n']:
+                    print(f'ValueError: value inputted must be y or n only.')
+                    continue
+        if inList:
+            if userInput.lower() not in inList:
+                    print(f'ValueError: value inputted must one of {inList}.')
+                    continue   
+        return userInput
 
