cb4ffd44dcaab86ff31d38d45fc7d5199b3c12c0
Cato Cannizzo <cannizzo@uw.edu>
2026-02-23 17:17:39 -0800

    Extended above X check for len of strings, cleaned arg passing

--- GeneralFunctions.py
+++ GeneralFunctions.py
@@ -1,6 +1,12 @@
-def promptUser(typed:type = None, userPrompt: str = None, aboveX:int = False, ynChoice: bool =False):
+def promptUser(typed:type = None, userPrompt: str = None, aboveX:int = None,underY:int = None, ynChoice: bool =False):
     """Returns value from prompted user, optional arg to enforce type return of prompt, and to give specific prompt"""
-    
+    kwargs = {
+        'typed':typed, 
+        'userPrompt':userPrompt, 
+        'aboveX':aboveX,
+        'underY':underY, 
+        'ynChoice': ynChoice
+    }
     if userPrompt:
         print(f"{userPrompt}")
     userInput = input()
@@ -10,18 +16,27 @@ def promptUser(typed:type = None, userPrompt: str = None, aboveX:int = False, yn
             userInput = typed(userInput)
         except ValueError:
             print(f'ValueError: value inputted must be of {typed.__name__} type')
-            return promptUser(typed, userPrompt, aboveX, ynChoice) 
+            return promptUser(**kwargs) 
         except Exception as e:
-            print(f'Unknown Error: {typed(e).__name__} - {e}')
-            return promptUser(typed, userPrompt, aboveX, ynChoice)   
+            print(f'Unknown Error: {type(e).__name__} - {e}')
+            return promptUser(**kwargs)   
         
-        if aboveX:
-            if not userInput > aboveX:
-                print(f'ValueError: value inputted must be greater than {aboveX}')
-                return promptUser(typed, userPrompt, aboveX, ynChoice) 
+    if isinstance(userInput, int):
+        lenInput = userInput
+        errorNoun = "value"
+    else:
+        lenInput = len(userInput)
+        errorNoun = "length"
+
+        if aboveX is not None and not (lenInput > aboveX):
+                print(f'ValueError: {errorNoun} of input must be greater than {aboveX}')
+                return promptUser(**kwargs) 
+        if underY is not None and not (lenInput < underY):
+            print(f'ValueError: {errorNoun} of input must be less than {underY}')
+            return promptUser(**kwargs)
         if ynChoice:
             if  userInput.lower() not in ['y','n']:
                 print(f'ValueError: value inputted must be y or n only.')
-                return promptUser(typed, userPrompt, aboveX, ynChoice)
+                return promptUser(**kwargs)
     return userInput
 
