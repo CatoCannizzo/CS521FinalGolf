189ca7d8616bee505148a881ab9b57536897ab6c
Cato Cannizzo <cannizzo@uw.edu>
2026-02-24 17:51:25 -0800

    Save and load tests

--- testGolf.py
+++ testGolf.py
@@ -1,4 +1,7 @@
 import unittest
+import json
+import os
+from Golf import load
 from GameObjects import *
 
 class testGolfRules(unittest.TestCase):
@@ -8,6 +11,11 @@ class testGolfRules(unittest.TestCase):
         self.j = Card('♠','J',True)
         self.a = Card('♠', 'A', True)
         self.two = Card('♠', '2', True)
+        self.cards = [self.k,self.q,self.a,self.k,self.j,self.two]
+        self.player = Player(self.cards, "TestPlayer")
+        self.deck = Deck()
+        self.game = Game([self.player],self.deck)
+        self.testSave = "dummySave.txt"
 
     def testCardValues(self):
         self.assertEqual(self.k.getValue(), 0)
@@ -17,13 +25,27 @@ class testGolfRules(unittest.TestCase):
         self.assertEqual(self.two.getValue(), 2)
 
     def testVertScore(self):
-        cards = [self.k,self.q,self.a,self.k,self.j,self.two]
-        p = Player(cards)
+        p = self.player
         self.assertEqual(p.calcScore(),23)
 
         cards2=[self.k,self.a,self.q,self.k,self.two,self.q]
         p2 = Player(cards2)
         self.assertEqual(p2.calcScore(),3)
+
+    def testSave(self):
+        self.game.save(self.testSave)
+        self.assertTrue(os.path.exists(self.testSave))
+
+    def testLoad(self):
+        loadedGame = Game(**load(self.testSave))
+        self.assertEqual(loadedGame.players[0].name, self.player.name)
+        for i in range(len(self.cards)):
+            self.assertEqual(loadedGame.players[0].getHand()[i].rank, self.player.getHand()[i].rank)
+        # !!! Create magic fun of __eq__ for Deck
+        self.assertEqual(len(loadedGame.discard), len(self.game.discard))
+        self.assertEqual(len(loadedGame.deck), len(self.game.deck))
+        self.assertEqual(loadedGame.currentPlayer, self.game.currentPlayer)
+
 
 if __name__ == '__main__':
     unittest.main()
